58871b2145bc16c208799f89a7baa7642417be60
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-05-12 14:54:16 -0300

    feat: função soma

--- Funções Array/array/app.js
+++ Funções Array/array/app.js
@@ -92,5 +92,38 @@ const tchurma = [
     {id: 85, name: "Zenon", age: 52},
 ]
 
-console.log(tchurma.every( p => p.age >= 18 ))
+//console.log(tchurma.every( p => p.age >= 18 ))
 
+
+/*  ================================================================================== 
+    some() 
+    =====================================================================================*/
+
+/*  Verificar se há algum número primo
+    =====================================================================================*/
+
+function isPrime( value ){
+    for(let i = 2; i < value; i++){
+        if( value % i === 0){
+            return false
+        }
+    }
+    return value > 1
+}
+
+const oneMoreArray = [ 6, 8, 12, 14, 42 ]
+
+//console.log( oneMoreArray.some( isPrime ))
+
+
+/*  Verificar por condições em valores/objetos
+    =====================================================================================*/
+
+const team = [
+    { id: 12, name: "Topper Harley", pilot: true },
+    { id: 44, name: "Ramada Thompson", pilot: true },
+    { id: 59, name: "Pete Thompson", pilot: false },
+    { id: 122, name: "Kowalski", pilot: false }
+]
+
+console.log(team.some( person => person.pilot))
