6ec0f50e9505eb3cac6c7313cd3417d2248273b2
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 12:41:12 -0300

    feat: o que é dom?

---
+++ programador espartano/dom/scripts.js
@@ -0,0 +1,27 @@
+/*  
+    *Adicionar, remover, mudar a forma as coisas como ela se comporta, manipular a página.
+
+    Conceito:
+
+    DOM - Document Object Model 
+    - Todo elemento que tem na página imagem, botão, campo de texto, div, qualquer coisa que tiver na página inclusive a
+    própria estrutura da página.
+    
+    - Cada item pode chamar de objeto
+
+    O que é o DOM?
+    - basicamente é a referência de cada elemento que estão aparecendo na página.
+
+    Manipular o DOM?
+    - O que isso significa... manipular os elementos que está aparecendo no processo de construção da página
+
+    Existe uma diferença entre DOM e Virutal DOM.
+
+    - Virtual DOM; um conjunto de elementos, não diretamente associados ao que está aparecendo na tela, ou seja,
+    é uma cópia da memória.
+*/
+
+
+
+
+
