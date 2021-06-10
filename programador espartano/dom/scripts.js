e8885d0e926585eb30cb2d23804685b477a810cf
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 17:16:29 -0300

    feat: distâncias e scroll suave

--- programador espartano/dom/scripts.js
+++ programador espartano/dom/scripts.js
@@ -5,9 +5,23 @@ let offAltura = document.querySelector('.texto').offsetHeight
 let cliLargura = document.querySelector('.texto').clientWidth
 let cliAltura = document.querySelector('.texto').clientHeight
 
+let textoTop = document.querySelector('.texto').scrollTop
+let textoLeft = document.querySelector('.texto').scrollLeft
+
+
+console.log("texto Top " +textoTop);
+console.log("texto Left " +textoLeft);
+
 console.log("Altura do cli " +cliAltura);
 console.log("Largura do cli " +cliLargura);
 console.log("Altura do off " +offAltura);
 console.log("Largura do off " +offLargura);
 console.log("Altura do Scroll " +altura);
 console.log("Largura do Scroll " +largura);
+
+let subirTela = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
