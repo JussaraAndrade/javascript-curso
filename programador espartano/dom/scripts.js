3ee6ae6fae944cc6d6f7288fe4d7991f69d04b10
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 14:09:59 -0300

    feat: selecionando com querySelector

--- programador espartano/dom/scripts.js
+++ programador espartano/dom/scripts.js
@@ -1,33 +1,13 @@
-// quatro principais formas selecionar alguns do elementos (objetos) da tela
+// identificar um elemento pelo id
+document.querySelector('#exemplo')
 
-// retorna apenas um elemento, é ilimitado não retorna mais de um elemento
-document.getElementById('exemplo')
+//identificar o elemento pela classe 
+document.querySelector('.exemplo')
 
-/*
-    - pega os elementos pelo nome da classe: class="lista"
-    - HTMLCollection; irá retornar um array com um item ou mais itens
-*/
+// selecionar mais de um elemento
+document.querySelectorAll('.lista')
 
-// selecionar ou alterar o primeiro item, seleciona o primeiro item do array [0]
-// document.getElementsByClassName('lista')[0].innerHTML= 'Item alterado!';
+document.querySelectorAll('.lista')[1].innerHTML = 'Alterado!';
 
-// segundo item 
-document.getElementsByClassName('lista')[1].innerHTML= 'Item alterado!';
+document.querySelector('button').innerHTML = 'Inscreva-se';
 
-// pelo nome da própria tag
-// document.getElementsByTagName('button')
-
-// pega item pelo nome, serve mais para campo de texto
-document.getElementsByName('email')
-document.getElementsByName('telefone')
-
-// Em resumo quatro principais, em ordem o mais ultilizado e o menos utilizando
-
-// mais usado
-document.getElementById('...')
-// bastante usado
-document.getElementsByClassName('...')
-// é generalista menos usado
-document.getElementsByTagName('...')
-// é muito pouco utilizado
-document.getElementsByName('...')
