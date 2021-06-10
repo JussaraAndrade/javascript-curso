31b53300beae4e29eb22a3be7441cab9d50e67e4
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 18:56:27 -0300

    feat: mudanças de estilos

--- programador espartano/conceitos-basicos/scripts.js
+++ programador espartano/conceitos-basicos/scripts.js
@@ -1,7 +1,25 @@
-let digitou = (event) => {
-  if(event.keyCode == 13 && event.ctrlKey == true) { // tecla Enter
-    let texto = document.getElementById('campo').value;
-    
-    console.log(texto);
-  }
+let azul = () => {
+    limpar();
+    document.getElementById("titulo").classList.add('azul');
+}
+
+let vermelho = () => {
+    limpar();
+    document.getElementById("titulo").classList.add('vermelho');
+}
+
+let verde = () => {
+    limpar();
+    document.getElementById("titulo").classList.add('verde');
+}
+
+let limpar = () => {
+    document.getElementById("titulo").classList.remove("azul");
+    document.getElementById("titulo").classList.remove("vermelho");
+    document.getElementById("titulo").classList.remove("verde");
+}
+
+let mostrarTelefone = (elemento) => {
+    elemento.style.display = 'none';
+    document.getElementById('telefone').style.display = "block";
 }
