2583d3a9aa3c110cb145eab962d3de940f5abade
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 11:20:30 -0300

    feat: condicionais parte 2

--- programador espartano/javascript1/scripts.js
+++ programador espartano/javascript1/scripts.js
@@ -1,11 +1,22 @@
 // caxinha de código 
-let hora = 8;
+let hora = 12;
 
 // condição If - condicional (para que alguma coisa tem que ser feita). precisa ser verdadeira para funcionar!
-if ( hora < 12 ){
-    console.log("Bom dia!");
-} else {
-    console.log("Não é de manhã!");
-}
+if ( hora == 12 || hora == 18) {
+    console.log("Você está na hora do rush");
+} 
+
+// if ( hora >= 12 && hora < 18) {
+//     console.log("Boa tarde");
+// }
+
+// if(hora < 12){
+//     console.log("Bom dia");
+// }
+// else if ( hora < 18 ) {
+//     console.log("Boa tarde");
+// } else if( hora <= 23 ) {
+//     console.log("Boa noite");
+// }
 
 
