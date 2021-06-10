cc44bd45c98c1af252ed4c67066e22c727dd6f60
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 17:28:55 -0300

    feat: ajustes scroll

--- programador espartano/dom/scripts.js
+++ programador espartano/dom/scripts.js
@@ -1,27 +1,22 @@
-let altura = document.querySelector('.texto').scrollHeight
-let largura = document.querySelector('.texto').scrollWidth
-let offLargura = document.querySelector('.texto').offsetWidth
-let offAltura = document.querySelector('.texto').offsetHeight
-let cliLargura = document.querySelector('.texto').clientWidth
-let cliAltura = document.querySelector('.texto').clientHeight
-
-let textoTop = document.querySelector('.texto').scrollTop
-let textoLeft = document.querySelector('.texto').scrollLeft
-
-
-console.log("texto Top " +textoTop);
-console.log("texto Left " +textoLeft);
-
-console.log("Altura do cli " +cliAltura);
-console.log("Largura do cli " +cliLargura);
-console.log("Altura do off " +offAltura);
-console.log("Largura do off " +offLargura);
-console.log("Altura do Scroll " +altura);
-console.log("Largura do Scroll " +largura);
-
 let subirTela = () => {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
 }
+
+let decidirBotaoScroll = () => {
+    if(window.scrollY === 0) {
+        //ocultar o botão
+        document.querySelector('.scrollbutton').style.display = 'none';
+
+    } else {
+        // caso ao contrário; mostrar o botão.
+        document.querySelector('.scrollbutton').style.display = 'block';
+    }
+}
+
+//setInterval(decidirBotaoScroll, 1000)
+
+// monitorando o scroll
+window.addEventListener('scroll', decidirBotaoScroll);
