e2dde196fabc70405e2aa0cabcf805bd83ec3e9f
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 13:27:31 -0300

    feat: selecionando elementos parte 2 - id, classe, tag do elemento html e pelo name

--- programador espartano/dom/scripts.js
+++ programador espartano/dom/scripts.js
@@ -1,5 +1,33 @@
 // quatro principais formas selecionar alguns do elementos (objetos) da tela
 
-// innerHTMl; significa dentro
-// trocar o texto
-document.getElementById('exemplo').innerHTML = '<h2>Jussara</h2> <button>Botão</button>';
+// retorna apenas um elemento, é ilimitado não retorna mais de um elemento
+document.getElementById('exemplo')
+
+/*
+    - pega os elementos pelo nome da classe: class="lista"
+    - HTMLCollection; irá retornar um array com um item ou mais itens
+*/
+
+// selecionar ou alterar o primeiro item, seleciona o primeiro item do array [0]
+// document.getElementsByClassName('lista')[0].innerHTML= 'Item alterado!';
+
+// segundo item 
+document.getElementsByClassName('lista')[1].innerHTML= 'Item alterado!';
+
+// pelo nome da própria tag
+// document.getElementsByTagName('button')
+
+// pega item pelo nome, serve mais para campo de texto
+document.getElementsByName('email')
+document.getElementsByName('telefone')
+
+// Em resumo quatro principais, em ordem o mais ultilizado e o menos utilizando
+
+// mais usado
+document.getElementById('...')
+// bastante usado
+document.getElementsByClassName('...')
+// é generalista menos usado
+document.getElementsByTagName('...')
+// é muito pouco utilizado
+document.getElementsByName('...')
