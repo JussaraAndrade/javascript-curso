d16424c88baa17323fb6c3021d9c074c380aac89
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 13:02:02 -0300

    feat: selecionando elementos parte 1 - pelo id

--- programador espartano/dom/scripts.js
+++ programador espartano/dom/scripts.js
@@ -1,27 +1,5 @@
-/*  
-    *Adicionar, remover, mudar a forma as coisas como ela se comporta, manipular a página.
+// quatro principais formas selecionar alguns do elementos (objetos) da tela
 
-    Conceito:
-
-    DOM - Document Object Model 
-    - Todo elemento que tem na página imagem, botão, campo de texto, div, qualquer coisa que tiver na página inclusive a
-    própria estrutura da página.
-    
-    - Cada item pode chamar de objeto
-
-    O que é o DOM?
-    - basicamente é a referência de cada elemento que estão aparecendo na página.
-
-    Manipular o DOM?
-    - O que isso significa... manipular os elementos que está aparecendo no processo de construção da página
-
-    Existe uma diferença entre DOM e Virutal DOM.
-
-    - Virtual DOM; um conjunto de elementos, não diretamente associados ao que está aparecendo na tela, ou seja,
-    é uma cópia da memória.
-*/
-
-
-
-
-
+// innerHTMl; significa dentro
+// trocar o texto
+document.getElementById('exemplo').innerHTML = '<h2>Jussara</h2> <button>Botão</button>';
