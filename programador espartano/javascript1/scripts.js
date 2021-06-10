8cc8b61895bb7224a07a4ff39404cb0eaa0f6325
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 11:04:14 -0300

    feat: condicionais

--- programador espartano/javascript1/scripts.js
+++ programador espartano/javascript1/scripts.js
@@ -1,28 +1,11 @@
-let x = 10;
-let y = 15;
+// caxinha de código 
+let hora = 8;
 
-let total = x + y;
-
-console.log(total);
-
-let nome = "Jussara";
-let idade = 27;
-console.log("Meu nome é " +nome +", tenho "+ idade+ " anos.");
-
-let total1 = nome+ " " +idade;
-console.log(total1);
+// condição If - condicional (para que alguma coisa tem que ser feita). precisa ser verdadeira para funcionar!
+if ( hora < 12 ){
+    console.log("Bom dia!");
+} else {
+    console.log("Não é de manhã!");
+}
 
 
-let nome1 = "Thomaz"; // STRING
-let sobrenome = "Henrique";
-
-let nomecompleto = nome1 + " " +sobrenome; // Concatena (juntar)
-
-console.log(nomecompleto);
-
-let x1 = "10"; // É diferente: let x1 = 10
-let y1 = "15"; // É diferente: let x2 = 15
-
-let total2 = x1 + y1;
-
-console.log(total2);
