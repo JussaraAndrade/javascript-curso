1ac8ceda5ba4603085b283624e6911fcc046f845
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 15:43:28 -0300

    feat: comentários

--- programador espartano/dom/scripts.js
+++ programador espartano/dom/scripts.js
@@ -1,3 +1,4 @@
+// definição das variáveis
 var nome = "Jussara"; // STRING
 var idade = 27; // NUMBER
 var salvo = true; // BOOLEAN
@@ -6,9 +7,14 @@ var cidade; // o tipo não foi definido. indefinido/undefined
 // saber o tipo da variável
 typeof nome
 
+/*
+    comentário de multiplas linhas
+*/
 var tipo = typeof idade;
 
+// Mostrando no Console o resultado
 console.log(tipo);
+
 
 
 
