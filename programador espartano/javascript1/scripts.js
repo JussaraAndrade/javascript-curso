58de79611084acbbabe3646a29e8daf1665952fe
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 10:39:32 -0300

    feat: formas de mostrar (output) com console, windows e document

--- programador espartano/javascript1/scripts.js
+++ programador espartano/javascript1/scripts.js
@@ -1,1 +1,12 @@
-alert("Opa, estou funcionando!");
+// quatro principais formas que mostra os dados para o usuário final
+
+// troca o texto para "Opa, tudo bem?"
+document.getElementById('titulo').innerHTML = "Opa, tudo bem?";
+// criando o próprio elemento
+document.write("Algum Texto");
+
+// janela do navegador
+window.alert("Mensagem de exemplo.");
+
+// erros, mensagem etc...
+console.log("A mensagem que eu inserir aqui, aparecerá no console.");
