012fdb5a1e764736ca08618fca89056a90a84d14
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 16:03:01 -0300

    feat: funções com parametro parte 2

--- programador espartano/dom/scripts.js
+++ programador espartano/dom/scripts.js
@@ -1,8 +1,18 @@
 // função é um conjunto de código
 
-function alterar() {
-    document.getElementById('titulo').innerHTML = "Trocou o titulo!";
-    document.getElementById('campo').value = "Trocou o campo!";
+function alterar(titulo) {
+    document.getElementById('titulo').innerHTML = titulo;
+    document.getElementById('campo').value = titulo;
 }
 
-alterar();
+function somar (x, y){
+    let total = x + y;
+
+    return total;
+
+}
+
+var resultado = somar(10, 15);
+
+console.log(resultado);
+
