7662b98901dcb77728ff8fd30cfbfa68b740ea69
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-10 16:37:31 -0300

    feat: manipulando atributos

--- programador espartano/dom/scripts.js
+++ programador espartano/dom/scripts.js
@@ -1,18 +1,9 @@
-// função é um conjunto de código
-
-function alterar(titulo) {
-    document.getElementById('titulo').innerHTML = titulo;
-    document.getElementById('campo').value = titulo;
+function trocarImagem(filename, animalname) {
+    document.querySelector('.imagem').setAttribute('src', 'images/' +filename);
+    document.querySelector('.imagem').setAttribute('data-animal', animalname);
 }
 
-function somar (x, y){
-    let total = x + y;
-
-    return total;
-
+function pegarAnimal(){
+    let animal = document.querySelector('.imagem').getAttribute('data-animal');
+    alert("O animal é: " +animal)
 }
-
-var resultado = somar(10, 15);
-
-console.log(resultado);
-
