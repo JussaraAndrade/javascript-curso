35afb3f17078b98be8159201b6db3f0f5a79ab87
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-11 11:10:44 -0300

    feat: objeto parte 1

--- programador espartano/conceitos-basicos/scripts.js
+++ programador espartano/conceitos-basicos/scripts.js
@@ -1,30 +1,28 @@
-// Array de lista
+// Objetos - é muito similar ao array
 
-let carros1 = ['Palio', 'Uno', 'Corolla', 'Ferrari'];
+/* Qual é a diferença entra array e objeto? 
 
-let carros2 = new Array('Palio', 'Uno', 'Corolla', 'Ferrari');
+    - Array; é uma listagem númerada
+    - Objeto; é uma listagem nomeada
+*/
 
-let carros3 = [
-    'Palio',   // 0
-    'Uno',     // 1
-    'Corolla', // 2
-    'Ferrari', // 3
-    // assuntos avançando
-    function() {
-        console.log('Testando 1, 2, 3...');
+// Objeto - ele tem 3 propriedades (nome, modelo e peso)
+let carros = {
+    nome: 'Fiat',
+    modelo: 'Uno',
+    peso: '800kg',
+    ligar: function() {
+        console.log("VRUM VRUM!");
+    },
+    acelerar: function () {
+        console.log("Riiiiiiiihihi");
     }
-];
+};
 
-carros3[4]();
+//console.log(carros['nome']);
+//console.log(carros.nome);
 
-let ingredientes = [
-    // Item 1
-    ['uva', 'pera', 'maça'],
-    // Item 2
-    ['arroz', 'macarrão']
-];
+console.log( carros.modelo );
 
-// pegou o arroz 
-console.log(ingredientes[1][0]);
-
-
+carros.ligar();
+carros.acelerar();
