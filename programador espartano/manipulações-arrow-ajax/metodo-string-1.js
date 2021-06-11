b9b04e7751ba21865485da54d688b6806d77ab65
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-11 15:07:09 -0300

    fix: alteração do nome arquivo js

---
+++ programador espartano/manipulações-arrow-ajax/metodo-string-1.js
@@ -0,0 +1,11 @@
+let nome = 'Jussara Andrade';
+
+let resultado = '';
+
+if(nome.indexOf('Jussara') > -1) {
+    resultado = 'Achou!';
+} else {
+    resultado = 'Não achou!'
+}
+
+console.log(resultado);
