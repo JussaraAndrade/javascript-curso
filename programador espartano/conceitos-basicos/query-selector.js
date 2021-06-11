b1fe43fdb275bdbd4c1954a35d6b7b8ad68ef90d
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-11 14:28:24 -0300

    feat: query selector (querySelectorAll)

---
+++ programador espartano/conceitos-basicos/query-selector.js
@@ -0,0 +1,19 @@
+let sub = document.getElementsByClassName('subtitulo');
+let sel = document.querySelector('.lista');
+let ol = document.querySelector('ol.lista');
+let til = document.querySelector('#titulo').innerHTML = 'Mudando o titulo';
+let lista = document.querySelectorAll('li');
+let body = document.querySelector('body')
+
+let color = document.querySelector('body').style.color = '#FF0000'
+
+// for(let i in lista){
+//     lista[i].style.color = '#FF0000';
+// }
+
+console.log(body);
+console.log(lis);
+console.log(til);
+console.log(ol);
+console.log(sel);
+console.log(sub);
