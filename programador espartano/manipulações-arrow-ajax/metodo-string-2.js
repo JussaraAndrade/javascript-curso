43761c423bfc8f343984eb5b2ff23d26e4f735db
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-11 15:06:08 -0300

    feat: métodos de string parte 2

---
+++ programador espartano/manipulações-arrow-ajax/metodo-string-2.js
@@ -0,0 +1,13 @@
+let nome = 'Jussara Andrade';
+
+// slice; pega posição 0 incial a 10 final
+// slice(-4); pega as ultimas string final
+let resultado1 = nome.slice(0, 10);
+
+let resultado2 = nome.substring(5, 10);
+
+let resultado3 = nome.substr(0, 5);
+
+console.log("Slice: " +resultado1);
+console.log("Substring: " +resultado2);
+console.log("Substr: " +resultado3);
