064c43aa4128c81290c9888bfb0eb5303ed86a0d
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-14 13:21:33 -0300

    feat: template strings

---
+++ programador espartano/manipulações-arrow-ajax/template-strings.js
@@ -0,0 +1,8 @@
+let nome = 'Thomaz';
+let idade = 90;
+
+let frase1 = 'Meu nome é ' +nome+ ', e eu tenho ' +idade+ ' anos e ano que vem eu farei ' +(idade + 1)+ ' anos.';
+let frase2 = `Meu nome é ${nome}, e eu tenho ${idade} anos e ano que vem eu farei ${idade+1} anos.`;
+
+console.log(frase1);
+console.log(frase2);
