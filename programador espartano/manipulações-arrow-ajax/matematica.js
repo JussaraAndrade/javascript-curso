c67f85ec60842049b505a69e167620d0a2be6932
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-14 11:31:32 -0300

    feat: matematica

---
+++ programador espartano/manipulações-arrow-ajax/matematica.js
@@ -0,0 +1,34 @@
+let novoValor1 = Math.PI;
+// arredonda o valor
+let novoValor2 = Math.round(3.47);
+
+// arredonda para baixo
+let novoValor3 = Math.floor(3.99);
+
+// arredonda para cima
+let novoValor4 = Math.ceil(3.1);
+
+// número absoluto
+let novoValor5 = Math.abs( -9.65464 );
+
+// vai dizer qual é o menor número
+let novoValor6 = Math.min(7, 100, 600, 20, 3);
+
+// vai dizer qual é o maior número
+let novoValor7 = Math.max(7, 100, 600, 20, 3);
+
+// retornar um número aleatório entre 0 e 1
+let novoValor8 = Math.floor(Math.random() * 100);
+
+// Math.cos
+// Math.tan
+// Math.sin
+
+console.log( novoValor1 );
+console.log( novoValor2 );
+console.log( novoValor3 );
+console.log( novoValor4 );
+console.log( novoValor5 );
+console.log( novoValor6 );
+console.log( novoValor7 );
+console.log( novoValor8 );
