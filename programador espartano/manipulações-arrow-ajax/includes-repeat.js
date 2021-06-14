283c9d477b8f747bd8b50825411d860784602d72
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-14 16:06:31 -0300

    feat: includes e repeat

---
+++ programador espartano/manipulações-arrow-ajax/includes-repeat.js
@@ -0,0 +1,11 @@
+let lista = ['ovo', 'café', 'arroz', 'feijão', 'macarrão'];
+
+console.log( lista.includes('ovo') );
+
+let nome = 'Thomaz';
+
+console.log( nome.includes('T') );
+
+console.log( 'x'.repeat(20) );
+
+console.log( nome.repeat(2) );
