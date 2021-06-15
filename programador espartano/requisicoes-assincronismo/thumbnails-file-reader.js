83ce40166402b1634203aae8288ca36998baa94a
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-15 15:27:55 -0300

    feat: thumbnails com filereader

---
+++ programador espartano/requisicoes-assincronismo/thumbnails-file-reader.js
@@ -0,0 +1,14 @@
+mostrar = () => {
+    let reader = new FileReader();
+    let imagem = document.getElementById('imagem').files[0];
+
+    reader.onloadend = (() => {
+        let img = document.createElement('img');
+        img.src = reader.result;
+        img.width = 200;
+
+        document.getElementById('area').appendChild(img);
+    })
+
+    reader.readAsDataURL(imagem);
+}
