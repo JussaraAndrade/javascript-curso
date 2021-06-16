876fc66d9fbdb33b6b3f914ea543d7c10f8a5cda
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-16 11:56:58 -0300

    feat: compra de pizzas - parte 7 - ações dos botões qtmais e qtmenos

--- programador espartano/projeto-compra-pizzas/js/script.js
+++ programador espartano/projeto-compra-pizzas/js/script.js
@@ -1,5 +1,6 @@
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
+let modalQt = 1;
 
 // Listagem das pizzas
 pizzaJson.map((item, index) => {
@@ -16,6 +17,8 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        // resetar para 1
+        modalQt = 1;
        
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -28,6 +31,8 @@ pizzaJson.map((item, index) => {
             }
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
         });
+
+        c('.pizzaInfo--qt').innerHTML = modalQt;
 
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
@@ -51,7 +56,21 @@ const closeModal = () => {
 cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
     item.addEventListener('click', closeModal);
 });
-
-
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if(modalQt > 1) {
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+   size.addEventListener('click', (e) => {
+    c('.pizzaInfo--size.selected').classList.remove('selected');
+    size.classList.add('selected');
+   });
+});
 
 
